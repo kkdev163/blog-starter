a52471ac9e7bdf02d1137155a5aee899f7803f6e
朱坤坤 <hzzhukunkun@corp.netease.com>
2019-03-28 20:46:26 +0800

    test commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `kkdev163 ' Blog`,
+    author: `ZhuKunKun`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
--- src/components/bio.js
+++ src/components/bio.js
@@ -38,12 +38,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              A personal blog by <strong>{author}</strong> who lives and works in HangZhou focusing on web development.
             </p>
           </div>
         )
